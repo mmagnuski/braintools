033db40f67c4e56444bd8e4d9419b684f0bff2a6
mmagnuski <mmagnuski@swps.edu.pl>
2015-02-04 17:36:12 +0100

    first commit, basic topoplot tools

---
+++ scale_topo_rim.m
@@ -0,0 +1,46 @@
+function scale_topo_rim(topo, scaleby)
+
+% changes the width of the EEGlab's topoplot rim
+% (one that covers ugly-looking edges and
+%  makes the plot look smooth) in the inward
+% direction
+% the topo passed to the function as input is
+% a structure of handles obtained by disecting
+% a topoplot with topo_scrapper
+% 
+% Example:
+% topo = topo_scrapper(topoplot_axis_handle);
+% scale_topo_rim(topo);
+% 
+% see also: topoplot, topo_scrapper
+
+if ~exist('scaleby', 'var')
+    scaleby = 2;
+end
+if ~(scaleby > 1)
+	error('currently scaleby must be more than one.');
+end
+
+% get patch xdata and ydata:
+x = get(topo.patch, 'XData');
+y = get(topo.patch, 'YData');
+
+rim_width = y(1) - y(end);
+rim_radius = y(end); % 
+target_width = rim_width * scaleby;
+
+% calculate
+add_width = target_width - rim_width;
+relative  = add_width / rim_radius;
+weight    = 1 - relative;
+
+num_points = length(x);
+half_num   = num_points / 2;
+
+x(half_num + 1 : end) = x(half_num + 1 : end) * weight + ...
+	repmat(0, [half_num, 1]) * relative;
+y(half_num + 1 : end) = y(half_num + 1 : end) * weight + ...
+	repmat(0, [half_num, 1]) * relative;
+
+set(topo.patch, 'XData', x);
+set(topo.patch, 'YData', y);
---
+++ topo_scrapper.m
@@ -0,0 +1,52 @@
+function h = topo_scrapper(ax)
+
+% TOPO_SCRAPPER disects EEGlab's topoplot axis structure
+% and exposes handles to easily navigate and manipulate
+% parts of the topoplot
+% 
+% warning! this works correct for normal filled
+% topoplot (was not tested for other topoplot types)
+
+
+% get children:
+chld = get(ax, 'Children');
+tps  = get(chld, 'Type');
+
+% test lines for being of none linestyle
+lns = chld(strcmp('line', tps));
+lnstl = get(lns, 'linestyle'); 
+elcs = strcmp('none', lnstl);
+shps = find(~elcs); elcs = find(elcs);
+
+% at the top are electrode markers
+h.elec_marks   = chld(elcs(1));
+if length(elcs) > 1
+	h.elec_marks2  = chld(elcs(2));
+end
+h.left_ear     = chld(shps(1));
+h.right_ear    = chld(shps(2));
+h.nose         = chld(shps(3));
+h.head         = chld(shps(4));
+
+% get electrode positions:
+flds = {'elec_pos', 'elec_pos2'; ...
+		'elec_marks', 'elec_marks2'};
+for i = 1:length(elcs)
+	h.(flds{1, i})    = [...
+		get(h.(flds{2,i}), 'X')', ...
+		get(h.(flds{2,i}), 'Y')', ...
+		get(h.(flds{2,i}), 'Z')'];
+end
+
+h.one_elec_group = length(elcs) == 1;
+
+
+% get title
+h.title       = get(ax, 'title');
+
+% patch and hggroup:
+h.patch = chld(find(strcmp('patch', tps)));
+h.hggroup = chld(find(strcmp('hggroup', tps)));
+
+% may be interesting to disect the hggroup into 
+% contour and image etc...
