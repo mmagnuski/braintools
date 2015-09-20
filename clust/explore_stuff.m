06ae63913e05af8f4949ef13d1dedfff0a790bff
mmagnuski <mmagnuski@swps.edu.pl>
2015-09-20 17:23:31 +0200

    update explore stuff

--- clust/explore_stuff.m
+++ clust/explore_stuff.m
@@ -1,17 +1,33 @@
 classdef explore_stuff < handle
     
-    % simple interface for exploring multi-dim data
+    % simple interface for exploring multi-dim effects
     properties
         h
         t
         dims
         last_str
         opt
+        stat
         EEG
     end
     
     methods
-        function obj = explore_stuff(EEG, t_val, effect_names)
+        function obj = explore_stuff(EEG, t_val, effect_names, varargin)
+            
+            % add support for stat
+            if isstruct(t_val)
+                obj.opt.hasstat = true;
+                obj.stat = t_val;
+                t_val = stat.stat;
+            else
+                obj.opt.hasstat = false;
+            end
+            
+            if nargin > 3
+                obj.fun.chan = vararigin{1};
+            end 
+
+            % init - topofigure
             obj.h.f1 = figure; obj.h.ax1 = axes('Parent', obj.h.f1);
             obj.dims = length(size(t_val));
             obj.opt.current_effect = 3;
@@ -28,9 +44,11 @@ classdef explore_stuff < handle
             else
                 topoplot([], EEG.chanlocs);
             end
+            obj.topo = topo_scrapper(gca);
             if obj.dims > 3
-                topo = topo_scrapper(gca);
-                set(topo.elec_labels, 'ButtonDownFcn', @(o, e) obj.show_elec());
+                set(obj.topo.elec_labels, 'ButtonDownFcn', @(o, e) obj.show_elec());
+            else
+                set(obj.topo.elec_labels, 'ButtonDownFcn', @(o, e) obj.show_elec_effect());
             end
             
             obj.EEG = EEG;
@@ -41,13 +59,15 @@ classdef explore_stuff < handle
             obj.h.drpdwn = uicontrol('style', 'popupmenu' , 'string', ...
                 effect_names, 'units', ...
                 'normalized', 'position', ...
-                [0.3, 0.0001, 0.2, 0.1], 'callback', ...
+                [0.3, 0.001, 0.2, 0.05], 'callback', ...
                 @(o, e) obj.change_effect(), 'value', 1);
             obj.h.winlims = uicontrol('style', 'edit' , 'string', ...
                 obj.last_str, 'units', ...
                 'normalized', 'position', ...
-                [0.6, 0.0001, 0.3, 0.1], 'callback', ...
+                [0.6, 0.001, 0.3, 0.05], 'callback', ...
                 @(o, e) obj.refresh_patch(), 'value', 1);
+            
+            obj.h.f3 = []; obj.h.ax3 = [];
             obj.draw_patch();
         end
         
@@ -99,10 +119,14 @@ classdef explore_stuff < handle
                 set(obj.opt.current_electrode_h, ...
                     'color', 'k');
             end
-            
+
             obj.opt.current_electrode = str2num(strelec); %#ok<ST2NM>
             obj.opt.current_electrode_h = gco;
-            obj.refresh_effect();
+            if obj.dims > 3
+                obj.refresh_effect();
+            else
+                obj.refresh_chanplot();
+            end
         end
 
         function change_effect(obj)
@@ -121,13 +145,37 @@ classdef explore_stuff < handle
             else
                 t = obj.t(:, :, obj.opt.current_effect);
             end
-            maskitsweet(t, abs(t) > obj.opt.t_threshold, ...
+            if obj.opt.hasstat
+                mask = get_cluster_mask(obj.stat, 0.05);
+            else
+                mask = abs(t) > obj.opt.t_threshold;
+            end
+            maskitsweet(t, mask, ...
                 'FigH', obj.h.f2, 'AxH', obj.h.ax2, ...
-                'Time', obj.EEG.times);
+                'Time', obj.EEG.times, 'nosig', 0.75);
             % redraw patch
             obj.last_str = '';
             obj.draw_patch();
-
+        end
+        
+        function refresh_chanplot(obj)
+            % create chan figure if absent
+            if isempty(obj.h.f3) || ~ishandle(obj.h.f3)
+                obj.h.f3 = figure;
+                obj.h.ax3 = axes;
+            else
+                axes(obj.h.ax3);
+                cla;
+            end
+            
+            % plot with default if no chanfun
+            if ~isempty(obj.fun.chan)
+                obj.fun.chan(obj.h.f3, obj.opt.current_electrode);
+            else
+                plot(obj.EEG.times, obj.t(obj.opt.current_electrode, :, ...
+                    obj.opt.current_effect));
+            end
+            
         end
     end
 end
