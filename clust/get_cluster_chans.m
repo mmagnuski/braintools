fce71e87ea6c86aaa5ac756730be3b379a07db2c
mmagnuski <mmagnuski@swps.edu.pl>
2015-10-21 17:35:23 +0200

    add help to get_cluster_chans

--- clust/get_cluster_chans.m
+++ clust/get_cluster_chans.m
@@ -1,5 +1,10 @@
 function chan = get_cluster_chans(clst, partic)
 
+% return a list of channel indices that constitute
+% the cluster, given participation criterion.
+% The participation criterion is minimum % of max
+% cluster time.
+
 if ~exist('partic', 'var')
     partic = 0.33;
 end
