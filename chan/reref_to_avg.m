405a1c97a593f04443ddaaa8e8d17418d2098641
mmagnuski <mmagnuski@swps.edu.pl>
2015-12-17 12:42:54 +0100

    rereference to average with little code

---
+++ chan/reref_to_avg.m
@@ -0,0 +1,12 @@
+function EEG = reref_to_avg(EEG)
+
+ref = EEG.chanlocs(1).ref;
+which_ref = strcmp(ref, {EEG.chaninfo.nodatchans.labels});
+if any(which_ref)
+	which_ref = find(which_ref);
+	ref_struct = EEG.chaninfo.nodatchans(which_ref(1));
+
+	EEG = pop_reref(EEG, [], 'keepref', 'on', 'refloc', ref_struct);
+else
+	error('Could not find original reference.');
+end
