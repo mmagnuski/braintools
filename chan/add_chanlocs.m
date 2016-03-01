4f294668a6047d99de21ff1d4d222e6557b26b72
mmagnuski <mmagnuski@swps.edu.pl>
2016-03-02 00:24:19 +0100

    make get_chanloc work for EasyCap too

--- chan/add_chanlocs.m
+++ chan/add_chanlocs.m
@@ -1,7 +1,21 @@
-function EEG = add_chanlocs(EEG)
+function EEG = add_chanlocs(EEG, captype)
 
 bt = fileparts(which('braintools'));
-chan_pth = fullfile(bt, 'chan', 'loc', 'GSN-HydroCel-65 1.0.sfp');
+captypes = {'EGI64', 'EasyCap64'};
+cap2fl = {'GSN-HydroCel-65 1.0.sfp', 'EasyCap_locs.txt'};
 
-EEG = pop_chanedit(EEG, 'load', chan_pth, 'changefield', ...
-	{68, 'datachan', 0}, 'setref', {'1:67', 'Cz'});
+if ~exist('captype', 'var') || isempty(captype)
+    captype = 'EGI64';
+end
+
+which_cap = find(strcmp(captype, captypes));
+locfile = cap2fl{which_cap}; %#ok<FNDSB>
+
+chan_pth = fullfile(bt, 'chan', 'loc', locfile);
+
+if strcmp(captype, 'EGI64')
+    EEG = pop_chanedit(EEG, 'load', chan_pth, 'changefield', ...
+        {68, 'datachan', 0}, 'setref', {'1:67', 'Cz'});
+elseif strcmp(captype, 'EasyCap64')
+    EEG = pop_chanedit(EEG, 'load', {chan_pth, 'filetype', 'sph'});
+end
