3a00662a176b5672e6bdac9c25a59fb82c1d23ba
mmagnuski <mmagnuski@swps.edu.pl>
2016-03-01 12:21:03 +0100

    better docstring for get_cluster_chans

--- clust/get_cluster_chans.m
+++ clust/get_cluster_chans.m
@@ -1,9 +1,20 @@
 function chan = get_cluster_chans(clst, partic, varargin)
 
 % return a list of channel indices that constitute
-% the cluster, given participation criterion.
-% The participation criterion is minimum % of max
-% cluster time.
+% the cluster, given percentage participation criterion.
+%
+% usage:
+% chan = get_cluster_chans(clst) % default 33% criterion
+% chan = get_cluster_chans(clst, partic)
+%
+% arguments:
+% clst   - cluster, structure returned by get_cluster
+% partic - participation criterion, minimum % of max
+%          cluster time.
+%
+% optional key-value arguments:
+% 'time' - vector of [min max] time range within which
+%          to get channels contributing to cluster
 
 if ~exist('partic', 'var')
     partic = 0.33;
