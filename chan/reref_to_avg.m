f3a7af84021df1352e67193ddb71a72ff1155ec0
mmagnuski <mmagnuski@swps.edu.pl>
2016-03-06 09:36:28 +0100

    add keepref option to reref

--- chan/reref_to_avg.m
+++ chan/reref_to_avg.m
@@ -1,12 +1,21 @@
-function EEG = reref_to_avg(EEG)
+function EEG = reref_to_avg(EEG, varargin)
 
-ref = EEG.chanlocs(1).ref;
-which_ref = strcmp(ref, {EEG.chaninfo.nodatchans.labels});
-if any(which_ref)
-	which_ref = find(which_ref);
-	ref_struct = EEG.chaninfo.nodatchans(which_ref(1));
+opt.keepref = true;
+if nargin > 1
+    opt = parse_arse(varargin, opt);
+end
 
-	EEG = pop_reref(EEG, [], 'keepref', 'on', 'refloc', ref_struct);
+if opt.keepref
+    ref = EEG.chanlocs(1).ref;
+    which_ref = strcmp(ref, {EEG.chaninfo.nodatchans.labels});
+    if any(which_ref)
+        which_ref = find(which_ref);
+        ref_struct = EEG.chaninfo.nodatchans(which_ref(1));
+        
+        EEG = pop_reref(EEG, [], 'keepref', 'on', 'refloc', ref_struct);
+    else
+        error('Could not find original reference.');
+    end
 else
-	error('Could not find original reference.');
+    EEG = pop_reref(EEG, []);
 end
