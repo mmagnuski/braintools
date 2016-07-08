62d7e256e38595ab9a5ad4ad4484de7dc282e2e2
mmagnuski <mmagnuski@swps.edu.pl>
2016-07-09 00:48:40 +0200

    get_chanconn now works with neighb structs

--- chan/get_chanconn.m
+++ chan/get_chanconn.m
@@ -1,6 +1,11 @@
 function chanconn = get_chanconn(captype, EEG)
 
-neighb = get_neighbours(captype);
+if ischar(captype)
+    neighb = get_neighbours(captype);
+elseif isstruct(captype)
+    neighb = captype;
+end
+
 if exist('EEG', 'var')
     cfg = get_cfg_for_chan_nb_mat(neighb, EEG);
 else
